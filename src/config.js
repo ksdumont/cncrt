8d3dd025d5fd6e7d214ae76537078adaf37e0f8d
ksdumont <ksdumont@gmail.com>
2020-03-13 13:47:17 -0700

    endpoint url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  API_BASE_URL: "http://localhost:8000" || process.env.REACT_APP_API_BASE_URL,
+  API_BASE_URL: "https://quiet-eyrie-75922.herokuapp.com"
 };
