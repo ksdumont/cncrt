252385e818e6f6364610f15ceb6f365fa21ee467
ksdumont <ksdumont@gmail.com>
2020-03-15 16:52:52 -0700

    added images

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -6,14 +6,18 @@ import CncrtContext from "../CncrtContext";
 class LandingPage extends Component {
   static contextType = CncrtContext;
   handleClick = e => {
-    e.preventDefault()
-    this.props.history.push('./results')
-  }
+    e.preventDefault();
+    this.props.history.push("./results");
+  };
   render() {
     return (
       <div className="landingPage">
         <nav className="landingNav">
-          <Link to="/">CNCRT</Link> |<Link to="/Login">Login</Link>
+          <button onClick={this.handleClick} className="bookButton">
+            Book Concert
+          </button>{" "}
+          <div className="line"> | </div>
+          <Link to="/Login">Login</Link>
         </nav>
 
         <h1 className="landingTitle">CNCRT</h1>
@@ -24,10 +28,23 @@ class LandingPage extends Component {
             directly.
           </h3>
         </section>
-        <section className="book-register">
-          <button onClick={this.handleClick} className="bookButton">Book Concert</button>
+        <section className="image-rows">
+          <img src="./images/image1.jpg" alt="album cover" />
+          <img src="./images/image2.jpg" alt="album cover" />
+          <img src="./images/image3.jpg" alt="album cover" />
+          <img src="./images/image7.jpg" alt="album cover" />
+          <img src="./images/image8.jpg" alt="album cover" />
+          <img src="./images/image10.jpg" alt="album cover" />
+          <img src="./images/image13.jpg" alt="album cover" />
+          <img src="./images/image15.jpg" alt="album cover" />
+          <img src="./images/image16.jpg" alt="album cover" />
+          <img src="./images/image17.jpg" alt="album cover" />
+          <img src="./images/image18.jpg" alt="album cover" />
+          <img src="./images/image19.jpg" alt="album cover" />
+        </section>
+        <section className="home-register">
           <Link className="artist-register-link" to="/Register">
-            Artist Register
+            Artist Register Here
           </Link>
         </section>
       </div>
